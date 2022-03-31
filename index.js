a038030ced21f1d527bcdfa753e9d529f9daec57
mattyicefire <81605759+mattyicefire@users.noreply.github.com>
2022-03-31 18:28:43 -0400

    show the details of comments

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ app.set('view engine', 'ejs')
 
 const comments = [
     {
+        id: 1,
         username: 'Todd',
         comment: 'lol that is funny'
     },
     {
+        id: 2,
         username: 'Charles',
         comment: 'this is cool'
     },
     {
+        id: 3,
         username: 'Tyler',
         comment: 'you are hurting my feelings'
     },
     {
+        id: 4,
         username: 'Matt',
         comment: 'lets pardy'
     }
@@ -38,6 +42,12 @@ app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment })
     res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    res.render('comments/show', { comment });
 })
 
 app.get('/tacos', (req, res) => {
