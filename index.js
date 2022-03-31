c2c04101386aa62dd701dc259d86437e56b5d39d
mattyicefire <81605759+mattyicefire@users.noreply.github.com>
2022-03-31 19:14:58 -0400

    sending a patch request to edit comments

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
+const { v4: uuid } = require('uuid');
+
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is funny'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Charles',
         comment: 'this is cool'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Tyler',
         comment: 'you are hurting my feelings'
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'Matt',
         comment: 'lets pardy'
     }
@@ -46,8 +51,22 @@ app.post('/comments', (req, res) => {
 
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment });
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
 })
 
 app.get('/tacos', (req, res) => {
